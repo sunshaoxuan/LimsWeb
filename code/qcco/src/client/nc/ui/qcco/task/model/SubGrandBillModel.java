5310d23b38620cc90c55928a3d3cbd4c5447156b
hepingyang <hepingyang>
2019-04-08 19:43:15 +0800

    任务单

---
+++ code/qcco/src/client/nc/ui/qcco/task/model/SubGrandBillModel.java
@@ -0,0 +1,10 @@
+package nc.ui.qcco.tak.model;
+
+import nc.ui.pubapp.uif2app.model.BillManageModel;
+
+public class SubGrandBillModel extends BillManageModel {
+
+	public void initModel(Object data) {
+		super.initModel(data);
+	}
+}
