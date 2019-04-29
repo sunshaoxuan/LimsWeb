2af1af704ebe63a5c22ec54711f089dd35a2006e
hepingyang <hepingyang>
2019-04-29 21:52:58 +0800

    参照过滤

--- code/qcco/src/client/nc/ui/qcco/commission/ace/handler/AceHeadTailBeforeEditHandler.java
+++ code/qcco/src/client/nc/ui/qcco/commission/ace/handler/AceHeadTailBeforeEditHandler.java
@@ -1,4 +1,4 @@
-package nc.ui.qcco.commission.ace.handler;
+package nc.ui.qcco.task.ace.handler;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +7,8 @@ import nc.ui.pub.beans.UIRefPane;
 import nc.ui.pub.bill.BillItem;
 import nc.ui.pubapp.uif2app.event.IAppEventHandler;
 import nc.ui.pubapp.uif2app.event.card.CardHeadTailBeforeEditEvent;
+import nc.ui.qcco.commission.refmodel.CommissionRefModel;
+import nc.ui.qcco.commission.refmodel.ProductContactRefModel;
 import nc.ui.qcco.commission.refmodel.ProductLastClassRefModel;
 import nc.ui.qcco.commission.refmodel.ProductSecondClassRefModel;
 
@@ -15,58 +17,23 @@ public class AceHeadTailBeforeEditHandler implements
 
 	@Override
 	public void handleAppEvent(CardHeadTailBeforeEditEvent e) {
-		if ("pk_subcategory".equals(e.getKey())) {
-			BillItem billItem = (BillItem) e.getSource();
-			((ProductSecondClassRefModel) ((UIRefPane) billItem.getComponent())
-					.getRefModel()).setPk_first_type(((UIRefPane) e
-					.getBillCardPanel().getHeadItem("pk_maincategory")
-					.getComponent()).getRefPK());
-		} else if ("pk_lastcategory".equals(e.getKey())) {
-			BillItem billItem = (BillItem) e.getSource();
-			((ProductLastClassRefModel) ((UIRefPane) billItem.getComponent())
-					.getRefModel()).setPk_second_type(((UIRefPane) e
-					.getBillCardPanel().getHeadItem("pk_subcategory")
-					.getComponent()).getRefPK());
-		} else if ("pk_dept".equals(e.getKey())) {
-			BillItem billItem = (BillItem) e.getSource();
-			((UIRefPane) billItem.getComponent()).getRefModel().setPk_org(
-					((UIRefPane) e.getBillCardPanel().getHeadItem("pk_owner")
-							.getComponent()).getRefPK());
-		} else if ("cuserid".equals(e.getKey())) {
-			BillItem billItem = (BillItem) e.getSource();
-			((UIRefPane) billItem.getComponent()).getRefModel().setPk_org(
-					e.getContext().getPk_org());
-		}else if("pk_commissiontype".equals(e.getKey())){
-			BillItem billItem = (BillItem) e.getSource();
-			String typeName = ((UIRefPane) billItem.getComponent()).getRefModel().getRefNameValue();
-			if(typeName != null){
-				String[] templates = CommissionShowTemplate.getTemplateByName(typeName);
-				String[] allTemplateFields = CommissionShowTemplate.getTemplateWithAllField();
-				Set<String> templatesSet = new HashSet();
-				
-				//先把模板字段设为null,如果是模板之外的,不清,反正是全部显示
-				//清空时,不清空此模板包含的字段
-				if(templates!=null && templates.length > 0){
-					for(String tmp : templates){
-						templatesSet.add(tmp);
-					}
-					for(String temp : allTemplateFields){
-						if(!templatesSet.contains(temp)){
-							e.getBillCardPanel().getHeadItem(temp).setValue(null);
-						}
-						
-					}
-				}
-				
-				e.getBillCardPanel().hideHeadItem(allTemplateFields);
-				if(templates == null){
-					templates = allTemplateFields;
-				}
-				e.getBillCardPanel().showHeadItem(templates);
-				
+		if ("pk_commission_h".equals(e.getKey())) {
+			if (e.getBillCardPanel().getHeadItem("pk_task_h").getValue()!=null) {
+				e.setReturnValue(false);
+				return;
 			}
+			BillItem billItem = (BillItem) e.getSource();
+			((CommissionRefModel) ((UIRefPane) billItem.getComponent())
+					.getRefModel()).setWherePart("1=1  and isnull(dr,0)=0 and QC_COMMISSION_H.BILLNO not in (select billno from qc_task_h where   billno is not null and dr != 1)");;
+					
+			UIRefPane pane = (UIRefPane) (e.getBillCardPanel().getHeadItem("pk_commission_h").getComponent());
+			CommissionRefModel refModel = (CommissionRefModel) (pane.getRefModel());
+			refModel.setCacheEnabled(false);
+			pane.setCacheEnabled(false);
+			
+			refModel.reloadData();
+			refModel.fireChange();
 		}
-
 		e.setReturnValue(true);
 	}
 }
