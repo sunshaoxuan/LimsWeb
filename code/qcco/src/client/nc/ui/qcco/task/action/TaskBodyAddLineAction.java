c6c43a8a77cfd6dbb4502f50fee1a7a0323cac44
hepingyang <hepingyang>
2019-04-29 23:10:39 +0800

    测试条件孙表

--- code/qcco/src/client/nc/ui/qcco/task/action/TaskBodyAddLineAction.java
+++ code/qcco/src/client/nc/ui/qcco/task/action/TaskBodyAddLineAction.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import nc.bs.dao.DAOException;
 import nc.bs.framework.common.NCLocator;
@@ -15,6 +16,7 @@ import nc.ui.pubapp.uif2app.actions.BodyAddLineAction;
 import nc.ui.pubapp.uif2app.view.ShowUpableBillForm;
 import nc.ui.qcco.task.view.SunlistPanel;
 import nc.vo.pub.BusinessException;
+import nc.vo.pub.lang.UFBoolean;
 import nc.vo.qcco.task.TaskBodyVO;
 import nc.vo.qcco.task.TaskHBodyVO;
 
@@ -64,10 +66,10 @@ public class TaskBodyAddLineAction extends BodyAddLineAction {
 								"runorder");
 						
 
+						insertTestCondition(pklists.get(i));
 					}
 				}
 				//生成孙表测试条件
-				insertTestCondition(pklists);
 			}
 		} catch (DAOException e) {
 			// TODO Auto-generated catch block
@@ -76,48 +78,116 @@ public class TaskBodyAddLineAction extends BodyAddLineAction {
 
 	}
 
-	private void insertTestCondition(List<TaskHBodyVO> pklists) {
-		List<String>list_NA = new ArrayList<>();
+	private void insertTestCondition(TaskHBodyVO taskHBodyVO) {
+		/*List<String>list_NA = new ArrayList<>();
 		List<String>nolist_NA = new ArrayList<>();
+		
+		InSQLCreator insql = new InSQLCreator();*/
 		IUAPQueryBS iUAPQueryBS = (IUAPQueryBS) NCLocator.getInstance().lookup(
 				IUAPQueryBS.class.getName());
-		InSQLCreator insql = new InSQLCreator();
 		try {
 		
-			for (TaskHBodyVO taskHBodyVO:pklists) {
+			//for (TaskHBodyVO taskHBodyVO:pklists) {
 				if(taskHBodyVO.getTestlistName().equals("_NA")){
-					list_NA.add(taskHBodyVO.getUnique());
+					List<Map<String, String>> custlist = (List<Map<String, String>>) iUAPQueryBS
+							.executeQuery(
+									"select cmp.name,cmp.OPTIONAL,cmp.REPORTABLE,cmp.RESULT_TYPE,cmp.UNITS,cmp.C_DEFAULT_VALUE,cmp.MINIMUM,"
+									+ "cmp.MAXIMUM,cmp.C_EN_DEFAULT_Value,ana.INSTRUMENT from nc_component_table cmp "
+									+ "inner join analysis ana on cmp.analysis = ana.name where analysis in (select TRIM(NC_TASK_ADDNAME) "
+									+ "NC_TASK_ADDNAME from nc_task_addunion where pk_task_addunion ='"+taskHBodyVO.getUnique()+"') ",
+									new MapListProcessor());
+					if(custlist != null && custlist.size() > 0){
+						for(Map<String, String> map : custlist){
+							this.getGrandCard().getBillCardPanel().getBodyPanel("pk_task_s").addLine();
+							int row = this.getGrandCard().getBillCardPanel().getRowCount("pk_task_s") - 1;
+							for(Entry<String, String> refValue : map.entrySet()){
+								if (refValue.getKey().equals("name")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("name"), row, "pk_testconditionitem", "pk_task_s");
+								} else if (refValue.getKey().equalsIgnoreCase("optional")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("optional").equals("T")?UFBoolean.TRUE:UFBoolean.FALSE, row, "isoptional", "pk_task_s");
+								} else if (refValue.getKey().equals("reportable")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("reportable").equals("T")?UFBoolean.TRUE:UFBoolean.FALSE, row, "isallow_out", "pk_task_s");
+								} else if (refValue.getKey().equals("result_type")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("result_type"), row, "pk_valuetype", "pk_task_s");
+								} else if (refValue.getKey().equals("units")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("units"), row, "unit", "pk_task_s");
+								} else if (refValue.getKey().equals("c_defvalue_value")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("c_defvalue_value"), row, "formatted_entry", "pk_task_s");
+								} else if (refValue.getKey().equals("minimum")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("minimum"), row, "min_limit", "pk_task_s");
+								} else if (refValue.getKey().equals("maximum")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("maximum"), row, "max_limit", "pk_task_s");
+								}else if (refValue.getKey().equals("c_en_default_value")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("c_en_default_value"), row, "englishdescription", "pk_task_s");
+								}else if (refValue.getKey().equals("instrument")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("instrument"), row, "instrument", "pk_task_s");
+								}
+								
+							}
+							if (this.getGrandCard().getBillCardPanel().getBodyValueAt(row, "refvalue") == null && this.getGrandCard().getBillCardPanel().getBodyValueAt(row, "textvalue") ==null) {
+								this.getGrandCard().getBillCardPanel().setBodyValueAt(1, row, "conditionstatus", "pk_task_s");
+							}
+							//this.getGrandCard().getBillCardPanel().setBodyValueAt(UFBoolean.TRUE, row, "conditionstatus", "pk_task_s");
+							
+						}
+					}
 					
 				}else {
-					nolist_NA.add(taskHBodyVO.getUnique());
+					List<Map<String, String>> custlists = (List<Map<String, String>>) iUAPQueryBS
+							.executeQuery(
+									"select NC_TESTLIST_COMP.NC_TESTCOMP_NAME,NC_TESTLIST_COMP.OPTIONAL,NC_TESTLIST_COMP.REPORTABLE,"
+									+ " NC_TESTLIST_COMP.PK_UNITS_TYPE,NC_TESTLIST_COMP.UNITS,NC_TESTLIST_COMP.C_DEFAULT_VALUE, "
+									+ " NC_TESTLIST_COMP.MIN_LIMIT,NC_TESTLIST_COMP.MAX_LIMIT,NC_TESTLIST_COMP.C_EN_DEFAULT_VALUE,"
+									+ " nc_result_type.pk_result_type,NC_RESULT_TYPE.nc_result_namecn,analysis.INSTRUMENT from NC_TESTLIST_COMP,NC_COMPONENT_table, "
+									+ " NC_RESULT_TYPE,analysis where NC_ANALYSIS_NAME = (select TRIM(NC_ANALYSIS_NAME) NC_ANALYSIS_NAME "
+									+ " from nc_analysis_list nal2 where nal2.name in (select distinct nal.nc_test_condition from "
+									+ " nc_analysis_list nal , nc_task_addunion nta where nal.nc_analysis_name = nta.nc_task_addname "
+									+ " and pk_task_addunion='"+taskHBodyVO.getUnique()+"' )) AND NC_TESTLIST_NAME = (select TRIM(NC_TESTLIST_NAME) NC_TESTLIST_NAME "
+									+ " from nc_task_addunion where pk_task_addunion='"+taskHBodyVO.getUnique()+"' ) and NC_TESTLIST_COMP.NC_ANALYSIS_NAME = "
+									+ " NC_COMPONENT_table.ANALYSIS and NC_TESTLIST_COMP.NC_TLC_COMPONENT = NC_COMPONENT_table.NAME "
+									+ " and NC_COMPONENT_table.pk_result_type= NC_RESULT_TYPE.pk_result_type and "
+									+ " NC_COMPONENT_table.analysis = analysis.name ",
+									new MapListProcessor());
+					if (custlists != null && custlists.size() > 0) {
+						for(Map<String, String> map : custlists){
+							this.getGrandCard().getBillCardPanel().getBodyPanel("pk_task_s").addLine();
+							int row = this.getGrandCard().getBillCardPanel().getRowCount("pk_task_s") - 1;
+							for(Entry<String, String> refValue : map.entrySet()){
+								if (refValue.getKey().equals("nc_testcomp_name")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("nc_testcomp_name"), row, "pk_testconditionitem", "pk_task_s");
+								} else if (refValue.getKey().equalsIgnoreCase("optional")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("optional").equals("T")?UFBoolean.TRUE:UFBoolean.FALSE, row, "isoptional", "pk_task_s");
+								} else if (refValue.getKey().equals("reportable")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("reportable").equals("T")?UFBoolean.TRUE:UFBoolean.FALSE, row, "isallow_out", "pk_task_s");
+								} else if (refValue.getKey().equals("pk_result_type")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("pk_result_type"), row, "pk_valuetype", "pk_task_s");
+								} else if (refValue.getKey().equals("units")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("units"), row, "unit", "pk_task_s");
+								} else if (refValue.getKey().equals("c_defvalue_value")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("c_defvalue_value"), row, "formatted_entry", "pk_task_s");
+								} else if (refValue.getKey().equals("min_limit")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("minimum"), row, "min_limit", "pk_task_s");
+								} else if (refValue.getKey().equals("max_limit")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("maximum"), row, "max_limit", "pk_task_s");
+								}else if (refValue.getKey().equals("c_en_default_value")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("c_en_default_value"), row, "englishdescription", "pk_task_s");
+								}else if (refValue.getKey().equals("nc_result_namecn")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("nc_result_namecn"), row, "valuetype", "pk_task_s");
+								}else if (refValue.getKey().equals("instrument")) {
+									this.getGrandCard().getBillCardPanel().setBodyValueAt(map.get("instrument"), row, "instrument", "pk_task_s");
+								}
+								}
+							if (this.getGrandCard().getBillCardPanel().getBodyValueAt(row, "refvalue") == null && this.getGrandCard().getBillCardPanel().getBodyValueAt(row, "textvalue") ==null) {
+								this.getGrandCard().getBillCardPanel().setBodyValueAt(1, row, "conditionstatus", "pk_task_s");
+							}
+							}
+					}
 				}
 			
-			}
-		String psInSQL = insql.getInSQL(list_NA.toArray(new String[list_NA.size()]));
-		String psInSQLs = insql.getInSQL(nolist_NA.toArray(new String[nolist_NA.size()]));
-		List<Map<String, String>> custlist = (List<Map<String, String>>) iUAPQueryBS
-				.executeQuery(
-						"select * from nc_component_table where analysis in "
-						+ "(select TRIM(NC_TASK_ADDNAME) NC_TASK_ADDNAME from "
-						+ "nc_task_addunion where pk_task_addunion in ("+psInSQL+")) ",
-						new MapListProcessor());
+			//}
+		//String psInSQL = insql.getInSQL(list_NA.toArray(new String[list_NA.size()]));
+		//String psInSQLs = insql.getInSQL(nolist_NA.toArray(new String[nolist_NA.size()]));
 		
-		List<Map<String, String>> custlists = (List<Map<String, String>>) iUAPQueryBS
-				.executeQuery(
-						"select * from NC_TESTLIST_COMP where NC_ANALYSIS_NAME in (select TRIM(NC_ANALYSIS_NAME) NC_ANALYSIS_NAME  "
-						+ "from nc_analysis_list nal2 where nal2.name in "
-						+ "(select distinct nal.nc_test_condition from nc_analysis_list nal , "
-						+ "nc_task_addunion nta  where nal.nc_analysis_name = nta.nc_task_addname "
-						+ "and pk_task_addunion='7204D0E9D70246BDBB0C')) AND NC_TESTLIST_NAME in (select TRIM(NC_TESTLIST_NAME)"
-						+ " NC_TESTLIST_NAME from nc_task_addunion where pk_task_addunion in("+psInSQLs+")); ",
-						new MapListProcessor());
-		if(custlist != null && custlist.size() > 0){
-			this.getGrandCard().getBillCardPanel().getBodyPanel("pk_task_s").addLine();
-		}
-		
-		if (custlists != null && custlists.size() > 0) {
-			
-		}
 		} catch (BusinessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
